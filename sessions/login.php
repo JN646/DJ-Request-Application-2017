856b2619e7486e2b38714de73fce8a30d22f6ef0
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 11:42:33 +0000

    Basic Login Form

---
+++ sessions/login.php
@@ -0,0 +1,16 @@
+<?php
+//Database Link
+?>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<form>
+	<label>Username:</label>
+	<input type="text"></input><br>
+	<label>Password:</label>
+	<input type="password"></input><br>
+	<button type="button">Login</button>
+	</form>
+</body>
