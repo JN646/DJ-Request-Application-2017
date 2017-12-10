14c86cd784f0fd8e66061f0a9b818c1b11186bc5
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 15:50:23 +0000

    Add template file
    + Template

---
+++ template.php
@@ -0,0 +1,30 @@
+<?php
+ /**
+  * Project:		DJ Request Application
+  * Copyright:		(C) JGinn 2017
+  * FileCreated:	171210
+  */
+	// Include config file
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
+	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
+?>
+<head>
+	<title>Song Management</title>
+</head>
+<body>
+	<div class="fluid-container">
+		<div class="col-md-12">
+			<div class="row">
+				<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/nav.php"); ?>
+				<div class="col-md-11">
+					<br>
+					<h1 class="display-4">Song Management</h1>
+					<ul>
+						<li><a href="#">Add Song</a></li>
+					</ul>
+				</div> <!-- Close col-md-11 -->
+			</div> <!-- Close row -->
+		</div> <!-- Close col-md-12 -->
+	</div> <!-- Close Container -->
+</body>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/footer.php"); ?>
