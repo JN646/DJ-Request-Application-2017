eeb24c2f734ea8ec6ae251af13a2ccfb4451f4db
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 14:32:04 +0000

    Basic Index layout

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
+<?php
+ /**
+  * Project:		DJ Request Application
+  * Copyright:		(C) JGinn 2017
+  * FileCreated:	171210
+  */
+	// Include config file
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
+	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
+?>
 <head>
 	<title>DJ App</title>
 </head>
 <body>
-	<h1>DJ App</h1>
-	<ul>
-		<li><a href="#">Admin</a></li>
-	</ul>
+	<div class="fluid-container">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-1">
+				<p>Link</p>
+				</div> <!-- Close col-md-1 -->
+				<div class="col-md-11">
+					<br>
+					<div class="jumbotron">
+						<h1>DJ App</h1>
+					</div>
+					<ul>
+						<li><a href="<?php echo $environment; ?>/sessions/login.php">Admin</a></li>
+					</ul>
+				</div> <!-- Close col-md-11 -->
+			</div> <!-- Close row -->
+		</div> <!-- Close col-md-12 -->
+	</div> <!-- Close Container -->
 </body>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/footer.php"); ?>
