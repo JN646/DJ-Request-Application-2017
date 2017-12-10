bb7aadc279a2daa00af25a953d4b7147422b2ca1
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 15:08:28 +0000

    Added Navigation
    + Nav links.

---
+++ partials/nav.php
@@ -0,0 +1,15 @@
+<?php
+ /**
+  * Project:		DJ Request Application
+  * Copyright:		(C) JGinn 2017
+  * FileCreated:	171210
+  */
+?>
+<div class="col-md-1">
+	<br>
+	<h3>ADMIN</h3>
+	<h5>Account</h5>
+	<ul class="nav flex-column">
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php">Dashboard</a></li>
+	</ul>
+</div>
