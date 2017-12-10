b20f917325f9f5db13a37510321a541271533034
JN646 <josh.ginn@hotmail.co.uk>
2017-12-10 15:53:03 +0000

    Link Updates
    Added new links in the header and song managment page to the home page.

--- partials/header.php
+++ partials/header.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">DJ Application</a>
+  <a class="navbar-brand" href="<?php echo $environment; ?>index.php">DJ Application</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
