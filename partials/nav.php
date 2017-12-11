415bbf91cf757dd8b38846e5dc8123b5a20c2094
JN646 <josh.ginn@hotmail.co.uk>
2017-12-11 17:50:48 +0000

    Replaced Navigation
    Added Config bar.

--- partials/nav.php
+++ partials/nav.php
@@ -10,20 +10,14 @@
 	<h3>ADMIN</h3>
 	<h5>Songs</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/index.php">Song Home</a></li>
 		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/list_song.php">Song List</a></li>
 	</ul>
-	<h5>Zones</h5>
+	<h5>Config</h5>
 	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>zones/zones_index.php">Zones Home</a></li>
-	</ul>
-	<h5>Sessions</h5>
-	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>sessions/sessions_index.php">Sessions Home</a></li>
-	</ul>
-	<h5>Clients</h5>
-	<ul class="nav flex-column">
-		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>clients/client_index.php">Clients Home</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/index.php">Songs</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>zones/zones_index.php">Zones</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>sessions/sessions_index.php">Sessions</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>clients/client_index.php">Clients</a></li>
 	</ul>
 	<h5>Account</h5>
 	<ul class="nav flex-column">
