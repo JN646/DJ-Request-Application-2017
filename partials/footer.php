565a52f8e31f03fb5aece48665408b3ca06889ad
JN646 <josh.ginn@hotmail.co.uk>
2017-12-16 16:11:26 +0000

    Update footer
    Change footer date.

--- partials/footer.php
+++ partials/footer.php
@@ -12,7 +12,7 @@
 		<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
 	</ul>
 	</small>
-	<small><p style="text-align: center">DJ System, build: 171210 for Life by J Ginn.</p></small>
+	<small><p style="text-align: center">DJ System, build: 171216 for Life by J Ginn.</p></small>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
