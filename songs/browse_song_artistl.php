d72b9d710f9e0d88935a95eff6ce569558e88bb6
JN646 <josh.ginn@hotmail.co.uk>
2017-12-17 23:14:23 +0000

    Improvements to song artist L
    Added more descriptive names.

--- songs/browse_song_artistl.php
+++ songs/browse_song_artistl.php
@@ -7,6 +7,7 @@
 	// Include config file
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
 	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/lib/lastfm.php");
 ?>
 <head>
 	<title>Song Artists</title>
@@ -18,16 +19,29 @@
 				<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/nav.php"); ?>
 				<div class="col-md-11">
 					<br>
-					<h1 class="display-4">Song Artists</h1>
+					<?php
+					$artist = urldecode($_GET['song_artist']);
+					echo "<h1 class='display-4'>$artist</h1>";
+					?>
 					<div id="status_bar" class="alert alert-warning" role="alert">This is a warning alert</div>
+					<br>
+					<div class="row">
 						<?php
-						$artist = urldecode($_GET['song_artist']);
 						// Attempt select query execution
 						$sql = "SELECT * FROM songs WHERE song_artist='$artist'";
 						if($result = mysqli_query($mysqli, $sql)){
 							if(mysqli_num_rows($result) > 0){
 								while($row = mysqli_fetch_array($result)){
-									echo "<p>" . $row['song_name'] . "</p>";
+									echo"<div class='col-md-2'>";
+										echo "<div class-'col-md-12 border' border-primary>";
+										echo "<img class='card-img-top' src=\"";
+										echo LastFMArtwork::getArtwork($row['song_artist'],$row['song_album'], true, "large");
+										echo "\">";
+										echo"<h4 class='text-center'>" . $row['song_name'] . "</h4>";
+										echo"<h5 class='text-center'>" . $row['song_artist'] . "</h5>";
+										echo"<p class='text-center'><a href=functions/update_count.php?song_id=" .$row['song_id']. ">Request</a></p>";
+										echo "</div>";
+									echo"</div>";
 								}
 								// Free result set
 								mysqli_free_result($result);
@@ -38,6 +52,7 @@
 							echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 						}
 						?>
+						</div>
 						<p><a href="browse_song_artist.php">Back</a></p>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
