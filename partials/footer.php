c18574121f01cd9466667efeb587333a44851d60
JN646 <josh.ginn@hotmail.co.uk>
2017-12-18 09:00:48 +0000

    Footer Update
    Changed the date, added an additional copyright notice and link to a terms and conditions page (not implemented).

--- partials/footer.php
+++ partials/footer.php
@@ -10,9 +10,10 @@
 	<small>
 	<ul class="text-center" style="list-style: none;">
 		<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
+		<li><a href="<?php echo $environment; ?>static/tandc.php">Terms & Conditions</a></li>
 	</ul>
 	</small>
-	<small><p style="text-align: center">DJ System, build: 171216 for Life by J Ginn.</p></small>
+	<small><p style="text-align: center">DJ Request System &copy; 2017, build: 171218 for Life by J Ginn.</p></small>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
