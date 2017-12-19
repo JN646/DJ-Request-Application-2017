c511eb8afedadaec104058523cc58964723d064f
JN646 <josh.ginn@hotmail.co.uk>
2017-12-19 21:07:48 +0000

    Added search to the Artist page

--- songs/search_song.php
+++ songs/search_song.php
@@ -7,6 +7,7 @@
 	// Include config file
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
 	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/lib/lastfm.php");
 ?>
 <head>
 	<title>Song Search</title>
@@ -18,8 +19,14 @@
 				<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/nav.php"); ?>
 				<div class="col-md-11">
 					<br>
-						<?php
-						echo "<h1 class='display-4'>Results for $_GET[search_val]</h1>";
+					<?php echo "<h1 class='display-4'>Results for $_GET[search_val]</h1>"; ?>
+					<form action="songs/search_song.php" method="get">
+						<div class="form-inline">
+							<input name="search_val" type="text" placeholder="Search" class="form-control" style="width: 60%"></input>
+							<button class="form-control btn btn-primary" name="SearchButton" value="search" type="submit">Search</button>
+						</div>
+					</form>
+					<?php
 						// Attempt select query execution
 						$sql = "SELECT * FROM songs
 								WHERE song_name LIKE '%$_GET[search_val]%'
@@ -30,6 +37,7 @@
 							if(mysqli_num_rows($result) > 0){
 								echo "<table class='table table-bordered'>";
 									echo "<tr>";
+										echo "<th width='48px'>Cover</th>";
 										echo "<th>Song</th>";
 										echo "<th>Artist</th>";
 										echo "<th>Year</th>";
@@ -38,6 +46,9 @@
 									echo "</tr>";
 								while($row = mysqli_fetch_array($result)){
 									echo "<tr>";
+										echo "<td><img class='card-img-top' onerror=this.src='images/250x250.png' src=\"";
+										echo LastFMArtwork::getArtwork($row['song_artist'],$row['song_album'], true, "small");
+										echo "\"></td>";
 										echo "<td>" . $row['song_name'] . "</td>";
 										echo "<td>" . $row['song_artist'] . "</td>";
 										echo "<td>" . $row['song_year'] . "</td>";
@@ -54,7 +65,7 @@
 						} else{
 							echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 						}
-						?>
+					?>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
 		</div> <!-- Close col-md-12 -->
