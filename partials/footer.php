f96aa258bc1bfe137543a121af5eb2322a8a4e80
JN646 <josh.ginn@hotmail.co.uk>
2017-12-20 19:20:50 +0000

    Add bottom space to footer.

--- partials/footer.php
+++ partials/footer.php
@@ -21,6 +21,7 @@
 			<img src="<?php echo $environment; ?>images/250x250.png" height="100px" width="250px">
 		</div>
 	</div>
+	<br>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
