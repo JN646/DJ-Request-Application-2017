5fb4e9a5305e9a4307beedadce1880c9d9c1d90d
JN646 <josh.ginn@hotmail.co.uk>
2017-12-20 15:15:15 +0000

    Footer powered by section.

--- partials/footer.php
+++ partials/footer.php
@@ -7,14 +7,20 @@
 ?>
 <footer>
 <hr>
-	<small>
-	<ul class="text-center" style="list-style: none;">
-		<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
-		<li><a href="<?php echo $environment; ?>static/tandc.php">Terms & Conditions</a></li>
-	</ul>
-	</small>
-	<small><p style="text-align: center">DJ Request System &copy; 2017, build: 171220 for Life by J Ginn.</p></small>
-
+	<div class="row">
+		<div class="col-md-8 offset-2">
+			<small>
+			<ul class="text-center" style="list-style: none;">
+				<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
+				<li><a href="<?php echo $environment; ?>static/tandc.php">Terms & Conditions</a></li>
+			</ul>
+			</small>
+			<small><p style="text-align: center">DJ Request System &copy; 2017, build: 171220 for Life by J Ginn.</p></small>
+		</div>
+		<div class="col-md-2">
+			<img src="<?php echo $environment; ?>images/250x250.php" width="100px">
+		</div>
+	</div>
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
