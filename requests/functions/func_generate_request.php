c82a80b1a76b4b71ac49a516a55ce8173abf1507
JN646 <josh.ginn@hotmail.co.uk>
2017-12-21 22:27:03 +0000

    Commented out lines
    Standardised the generate request code.

--- requests/functions/func_generate_request.php
+++ requests/functions/func_generate_request.php
@@ -8,11 +8,11 @@
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
 	
 	//Attempt insert query execution
-	$update = "UPDATE requests SET request_active = 0 WHERE request_id='$_GET[request_id]'";
+	//$update = "UPDATE requests SET request_active = 0 WHERE request_id='$_GET[request_id]'";
 
 	//Execute the Query
 	if(mysqli_query($mysqli,$update))
-			header("refresh:0; url=../request_index.php");
+			header("refresh:0; url=../index.php");
 		else
 			echo "Not Updated";
 	 
