6d8467a5d328987ea1dd8502b086d3f5ee38a786
JN646 <josh.ginn@hotmail.co.uk>
2017-12-21 16:34:11 +0000

    Update zone redirect code

--- zones/functions/func_update_zone.php
+++ zones/functions/func_update_zone.php
@@ -7,4 +7,10 @@
 	// Include config file
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
 	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
+	
+	//Execute the sql
+if(mysqli_query($mysqli,$sql))
+		header("refresh:0; url=../zones_index.php");
+	else
+		echo "Not Updated";
 ?>
