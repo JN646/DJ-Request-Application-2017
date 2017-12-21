a0d30e87510276232e5bde0192ee1481d40aa0e2
JN646 <josh.ginn@hotmail.co.uk>
2017-12-21 21:53:44 +0000

    Generate Request Template

---
+++ requests/functions/func_generate_request.php
@@ -0,0 +1,21 @@
+<?php
+ /**
+  * Project:		DJ Request Application
+  * Copyright:		(C) JGinn 2017
+  * FileCreated:	171222
+  */
+	// Include config file
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBconfig.php");
+	
+	//Attempt insert query execution
+	$update = "UPDATE requests SET request_active = 0 WHERE request_id='$_GET[request_id]'";
+
+	//Execute the Query
+	if(mysqli_query($mysqli,$update))
+			header("refresh:0; url=../request_index.php");
+		else
+			echo "Not Updated";
+	 
+	// close connection
+	mysqli_close($mysqli);
+?>
