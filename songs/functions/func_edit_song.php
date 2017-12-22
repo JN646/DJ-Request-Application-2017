42e00d23fea803c06787ebb48866fb996ace86b3
JN646 <josh.ginn@hotmail.co.uk>
2017-12-22 07:59:57 +0000

    Edit song tweaks
    Mde some changes to the edit song template.

--- songs/functions/func_edit_song.php
+++ songs/functions/func_edit_song.php
@@ -2,7 +2,7 @@
  /**
   * Project:		DJ Request Application
   * Copyright:		(C) JGinn 2017
-  * FileCreated:	171210
+  * FileCreated:	171222
   */
 	// Include config files
 	require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/config/DBVar.php");
@@ -10,7 +10,7 @@
 	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
 ?>
 <head>
-	<title>Template</title>
+	<title>Edit Song</title>
 </head>
 <body>
 	<div class="fluid-container">
@@ -19,20 +19,33 @@
 				<?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/nav.php"); ?>
 				<div class="col-md-11">
 					<br>
-					<h1 class="display-4">Edit Template</h1>
+					<?php
+					$artist = "Edit " . urldecode($_GET['song_id']);
+					echo "<h1 class='display-4'>$artist</h1>";
+					?>
 					<p>Default edit text goes here.</p>
 					<?php
 					$UID = (int)$_GET["song_id"];
-					$query = mysql_query("SELECT * FROM songs WHERE song_id = '$UID'");
+					$query = mysqli_query("SELECT * FROM songs WHERE song_id = '$UID'");
 					
-					if(mysql_num_rows($query)>=1) {
-						while($row = mysql_fetch_array($query)) {
-							$song_name = $row['song_name'];
-							$artist = $row['song_artist'];
-							$album = $row['song_album'];
+					if($query = mysqli_query($mysqli, $query)){
+						if(mysqli_num_rows($query) > 0){
+							while($row = mysqli_fetch_array($query)){
+								$song_name = $row['song_name'];
+								$artist = $row['song_artist'];
+								$album = $row['song_album'];
+							}
+							// Free query set
+							mysqli_free_result($query);
+						} else{
+							echo "<p>No songs were found.</p>";
+							echo '<a href="javascript:history.back()">Go back</a>';
 						}
+					} else{
+						echo "ERROR: Not able to execute $query. " . mysqli_error($mysqli);
+						echo '<a href="javascript:history.back()">Go back</a>';
+					}
 					?>
-					
 					<h2>Form:</h2>
 					<form action="update.php" method="post">
 						<input type="hidden" name="ID" value="<?=$UID;?>">
@@ -42,9 +55,9 @@
 						<input type="Submit">
 					</form>
 					<?php
-					} else {
-						echo 'No entry found. <a href="javascript:history.back()">Go back</a>';
-					}
+					// } else {
+						// echo 'No entry found. <a href="javascript:history.back()">Go back</a>';
+					// }
 					?>
 					
 				</div> <!-- Close col-md-11 -->
