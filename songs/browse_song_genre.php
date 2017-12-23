cfadbab149ede171c77a3ebf3f723b43fd10e811
JN646 <josh.ginn@hotmail.co.uk>
2017-12-23 08:47:43 +0000

    Tidy browse by genre
    Tidy browse by genre and limit SQL from displaying blank values.

--- songs/browse_song_genre.php
+++ songs/browse_song_genre.php
@@ -20,13 +20,16 @@
 					<br>
 					<h1 class="display-4">Song Genres</h1>
 					<div id="status_bar" class="alert alert-warning" role="alert">This is a warning alert</div>
+					<div class="row">
 						<?php
 						// Attempt select query execution
-						$sql = "SELECT DISTINCT song_genre FROM songs";
+						$sql = "SELECT DISTINCT song_genre FROM songs WHERE song_genre <> '' ORDER BY song_genre ASC";
 						if($result = mysqli_query($mysqli, $sql)){
 							if(mysqli_num_rows($result) > 0){
 								while($row = mysqli_fetch_array($result)){
-									echo "<p>" . $row['song_genre'] . "</p>";
+									echo "<div class='col-md-2'>";
+										echo "<h3>" . $row['song_genre'] . "</h3>";
+									echo "</div>";
 								}
 								// Free result set
 								mysqli_free_result($result);
@@ -37,6 +40,7 @@
 							echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 						}
 						?>
+					</div>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
 		</div> <!-- Close col-md-12 -->
