1a45f2c8ecfd5a8f17093dc0bfac79ad5d08c0fb
JN646 <josh.ginn@hotmail.co.uk>
2017-12-23 17:56:00 +0000

    Request song on image click
    Added click to request to image blocks.

--- index.php
+++ index.php
@@ -51,9 +51,9 @@
 							while($row = mysqli_fetch_array($result)){
 								echo"<div class='col-md-2'>";
 									echo "<div class-'col-md-12 border' border-primary>";
-									echo "<img class='card-img-top' onerror=this.src='images/250x250.png' src=\"";
+									echo "<a href='functions/update_count.php?song_id=" .$row['song_id']. "'><img class='card-img-top' onerror=this.src='images/250x250.png' src=\"";
 									echo LastFMArtwork::getArtwork($row['song_artist'],$row['song_album'], true, "large");
-									echo "\">";
+									echo "\"></a>";
 									echo"<h4 class='text-center'>" . $row['song_name'] . "</h4>";
 									echo"<h5 class='text-center'>" . $row['song_artist'] . "</h5>";
 									echo"<p class='text-center'><a href=functions/update_count.php?song_id=" .$row['song_id']. ">Request</a></p>";
