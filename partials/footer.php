66237fe85d37b941d19fb9f5821b23bdc5c3361a
JN646 <josh.ginn@hotmail.co.uk>
2017-12-24 09:21:31 +0000

    Footer tweaks

--- partials/footer.php
+++ partials/footer.php
@@ -7,18 +7,28 @@
 ?>
 <footer>
 <hr>
-	<div class="row">
-		<div class="col-md-8 offset-2">
-			<small>
-			<ul class="text-center" style="list-style: none;">
-				<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
-				<li><a href="<?php echo $environment; ?>static/tandc.php">Terms & Conditions</a></li>
-			</ul>
-			</small>
-			<small><p style="text-align: center">DJ Request System &copy; 2017, build: 171223 for Life by J Ginn.</p></small>
-		</div>
-		<div class="col-md-2">
-			<img src="<?php echo $environment; ?>images/bottom_logo.png" alt="Logo" align="middle" height="30%">
+	<div class="container">
+		<div class="row">
+			<table class="table-footer">
+				<tr>
+					<td>
+						<small>
+						<ul class="text-center" style="list-style: none;">
+							<li><a href="<?php echo $environment; ?>static/about.php">About</a></li>
+							<li><a href="<?php echo $environment; ?>static/features.php">Features</a></li>
+							<li><a href="<?php echo $environment; ?>static/help.php">Help</a></li>
+							<li><a href="<?php echo $environment; ?>static/tandc.php">Terms & Conditions</a></li>
+						</ul>
+						</small>
+					</td>
+				</tr>
+				<tr>
+					<td><small><p class="text-center">DJ Request System &copy; 2017, build: 171224 for Life by J Ginn.</p></small></td>
+				</tr>
+				<tr>
+					<td><img src="<?php echo $environment; ?>images/bottom_logo.png" alt="Logo" height="30%"></td>
+				</tr>
+			</table>
 		</div>
 	</div>
 	<br>
