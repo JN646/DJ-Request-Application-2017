1d31790847eead71c9e34ed09230a06dcf1adc58
Darius Alexander <dariusjalexander@gmail.com>
2017-12-26 18:38:51 +0000

    Changes to request system

--- requests/functions/func_add_request.php
+++ requests/functions/func_add_request.php
@@ -10,29 +10,19 @@
 	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
 
 	$UID = (int)$_GET["song_id"];
-	$sql = "INSERT INTO requests (request_song_id) VALUES ('$UID')";	
-	$terms = "SELECT * FROM songs WHERE song_id = '$UID'";
-	$query = mysqli_query($mysqli, $terms);
-
-	// if(mysqli_num_rows($query) > 0){
-		// while($row = mysqli_fetch_array($query)){
-			// $song_name = $row['song_name'];
-			// $artist = $row['song_artist'];
-			// $album = $row['song_album'];
-			// $genre = $row['song_genre'];
-			// $year = $row['song_year'];
-		// }
-	//Attempt insert query execution
 	$sql = "INSERT INTO requests (request_song_id) VALUES ('$UID')";
-
-if(mysqli_query($mysqli,$sql))
+	mysqli_query($mysqli, $sql);
+	echo "Affected rows: ".mysqli_affected_rows($mysqli);
+	
+	if(mysqli_query($mysqli, $sql)) { //don't know what this does -doesn't seem to be testing for successful insert
 		header("refresh:0; url=../request_index.php");
-	else
+	} else {
 		echo "<br>";
 		echo "<div class='fluid-container'>";
 			echo "<h1>Song Requests</h1>";
 			echo "<p>Not Requested</p>";
 		echo "</div>";
+	}
 		
 // close connection
 mysqli_close($mysqli);
