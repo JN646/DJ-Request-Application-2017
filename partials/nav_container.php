869731ab7bd4ca76a0eac67da064692aca80f2d7
JN646 <josh.ginn@hotmail.co.uk>
2017-12-27 14:22:56 +0000

    Nav bar only appears when user is logged in

--- partials/nav_container.php
+++ partials/nav_container.php
@@ -4,11 +4,24 @@
 * Copyright:		(C) JGinn 2017
 * FileCreated:	171227
 */
-
-if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
-	// If not logged in.
-} else {
-	// If logged in.
-	include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/nav.php");
-}
 ?>
+<div class="col-md-1" style="min-width: 100px">
+	<br>
+	<h3>ADMIN</h3>
+	<h5>Songs</h5>
+	<ul class="nav flex-column">
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>requests/request_index.php">My Requests</a></li>
+	</ul>
+	<h5>Config</h5>
+	<ul class="nav flex-column">
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/index.php">Songs</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>zones/zones_index.php">Zones</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>sessions/sessions_index.php">Sessions</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>clients/client_index.php">Clients</a></li>
+	</ul>
+	<h5>Admin</h5>
+	<ul class="nav flex-column">
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php">Dashboard</a></li>
+		<li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>accounts/logout.php">Logout</a></li>
+	</ul>
+</div>
