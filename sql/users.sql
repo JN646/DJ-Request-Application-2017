a9438a866292a0cfd26a208572e3d02f679fb498
JN646 <josh.ginn@hotmail.co.uk>
2017-12-27 13:32:44 +0000

    Added new Users test db

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 27, 2017 at 12:25 PM
+-- Generation Time: Dec 27, 2017 at 01:31 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.9
 
@@ -33,18 +33,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `djname` varchar(100) DEFAULT NULL COMMENT 'Store the users DJ Name',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `current_session_id` int(11) DEFAULT NULL COMMENT 'Currently running session attached to this user. ',
   `is_admin` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `is_admin`) VALUES
-(7, 'Josh', '$2y$10$Ry2zVNurJh/cPzg8uTsbYeirkwrcFgtq6ncGmIK5E/YqjN0wHKunS', '2017-12-27 11:44:55', 0);
+INSERT INTO `users` (`id`, `username`, `password`, `djname`, `created_at`, `current_session_id`, `is_admin`) VALUES
+(7, 'Josh', '$2y$10$Ry2zVNurJh/cPzg8uTsbYeirkwrcFgtq6ncGmIK5E/YqjN0wHKunS', 'The Badger', '2017-12-27 11:44:55', 1, 1),
+(8, 'Test Account', '$2y$10$vLeO06vkijOP30uG1NyL0OQSCbwVqvhivzmZmaRRhyKmj37.JgSzq', 'The Woman', '2017-12-27 12:57:55', 1, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
