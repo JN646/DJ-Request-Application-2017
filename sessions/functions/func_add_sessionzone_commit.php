5b0957677203d0272ae1abd3f60ca817ffc6fb37
JN646 <josh.ginn@hotmail.co.uk>
2017-12-27 20:25:29 +0000

    Added some functionality

--- sessions/functions/func_add_sessionzone_commit.php
+++ sessions/functions/func_add_sessionzone_commit.php
@@ -10,21 +10,33 @@
 if ($_POST['add_session'] == 'Submit') {
 	
 	// SELECT all of the zone_id from zone db.
+	$terms = "SELECT * FROM zones";
+	$result = mysqli_query($mysqli,$terms);
+	$rs = mysqli_fetch_array($result);
+	$zoneid = (int)$rs['zone_id'];
+	
 	// Store the number of IDs into a variable
 	
 	// FOR LOOP - Keep looping while i < zonecount
-	$Zones = mysqli_real_escape_string($mysqli, $_REQUEST['ZONE ID']);
+	while($row = mysqli_fetch_array($result)){
 
-	// LOOP into SQL
-	$sql = "INSERT INTO songs (song_name, song_artist, song_album, song_year, song_genre) VALUES ('$song_name', '$song_artist', '$song_album', '$song_year', '$song_genre')";
-	
-	// End Loop
+		// LOOP into SQL
+		$sql = "INSERT INTO songs (song_name, song_artist, song_album, song_year, song_genre) VALUES ('$song_name', '$song_artist', '$song_album', '$song_year', '$song_genre')";
+	}
 }
 
-if(mysqli_query($mysqli,$sql))
-		header("refresh:0; url=../add_song.php");
-	else
+
+if(mysqli_query($mysqli,$sql1)) {
+		// query 1
+} else {
 		echo "Not added. Something went wrong.";
+}
+
+if(mysqli_query($mysqli,$sql2)) {
+		// query 2
+} else {
+		echo "Not added. Something went wrong.";
+}
  
 // close connection
 mysqli_close($mysqli);
