209702ddcc897a89adb30fbb9108d471fc81f583
JN646 <josh.ginn@hotmail.co.uk>
2018-01-07 21:22:27 +0000

    Added comments and new variables

--- config/DBVar.php
+++ config/DBVar.php
@@ -1,11 +1,17 @@
 <?php
 /**
 * Project:		DJ Request Application
-* Copyright:	(C) JGinn 2017
+* Copyright:	(C) JGinn 2017 - 2018
 * FileCreated:	171210
 */
 
-//Global Variables.
+/* 
+This file loads all of the global variables for the application. When you install the system, ensure you go through this file and make sure all of the correct variables and details have been added to this file.
+
+Make a backup of this file as changes can have catastrophic results. 
+ */
+
+// Global Variables.
 define("LOCAL", "http://localhost/djx/djx/"); //local URL
 define("WEB", "http://foo.bar"); //website URL
 $environment = LOCAL; //change to WEB if you're live
@@ -13,7 +19,7 @@ $environment = LOCAL; //change to WEB if you're live
 // Venue Details
 // Enter the details of your venue, these variables can be loaded in the application.
 $VenueName = "My Venue";
-$VenueSlogan = "The best venue. This text is loaded from a config file.";
+$VenueSlogan = "The best venue. This text is loaded dynamically from a config file.";
 $VenueType = "Club";
 
 // Venue opening and closing times.
@@ -22,4 +28,10 @@ $VenueCloseTime = "0400";
 
 // Kiosk closed message
 $KioskClosedMessage = "Sorry but this kiosk is closed.";
+
+// Social Media
+$FacebookAccount = "http://facebook.com/google";
+$TwitterAccount = "@test";
+$SnapchatAccount = "Test"
+$VenuePhone = "01234 123456";
 ?>
