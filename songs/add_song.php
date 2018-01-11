95b358c85fe068ad5ca1ff4d89ec84cf2cbc9f6b
JN646 <josh.ginn@hotmail.co.uk>
2018-01-11 22:56:31 +0000

    Started adding JS validation
    Add validation for the year and title fields of the add new song page.

--- songs/add_song.php
+++ songs/add_song.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Project:		DJ Request Application
-* Copyright:		(C) JGinn 2017
+* Copyright:	(C) JGinn 2017
 * FileCreated:	171210
 */
 // Include config file
@@ -17,6 +17,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 	<title>Add Song</title>
 </head>
 <body>
+	<body onload="Check_Empty()">
 	<div class="fluid-container">
 		<div class="col-md-12">
 			<div class="row">
@@ -24,20 +25,20 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 				<div class="col-md-11">
 					<br>
 					<h1 class="display-4">Add Song</h1>
-					<p>Manually add a song to the database.</p>
 					<div id="status_bar" class="alert alert-warning" role="alert">This is a warning alert</div>
+					<p>Manually add a song to the database.</p>
 					<form class="col-md-6" action="<?php echo $environment; ?>songs/functions/func_add_song.php" method="post">
 						<div class="form-group">
 							<label>Song Name</label>
-							<input name="song_name" class="form-control" placeholder="Song Name" type="text"></input>
+							<input name="song_name" id="song_name" class="form-control" placeholder="Song Name" type="text"></input>
 						</div>
 						<div class="form-group">
 							<label>Song Artist</label>
-							<input name="song_artist" class="form-control" placeholder="Song Artist" type="text"></input>
+							<input name="song_artist" id="song_artist" class="form-control" placeholder="Song Artist" type="text"></input>
 						</div>
 						<div class="form-group">
 							<label>Song Album</label>
-							<input name="song_album" class="form-control" placeholder="Song Album" type="text"></input>
+							<input name="song_album" id="song_album" class="form-control" placeholder="Song Album" type="text"></input>
 						</div>
 						<div class="form-row">
 						<div class="col-md-6">
@@ -54,10 +55,10 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 						</div>
 						<div class="col-md-6">
 							<label>Song Year</label>
-							<input name="song_year" class="form-control" placeholder="Song Year" type="text"></input>
+							<input name="song_year" id="song_year" class="form-control" placeholder="Song Year" type="text"></input>
 						</div>
 						</div>
-						<button class="btn btn-primary" name="add_song" type="submit" value="Submit">Submit</button>
+						<button class="btn btn-primary" name="add_song" id="submit_button" type="submit" value="Submit">Submit</button>
 					</form>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
@@ -68,5 +69,40 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 // hide status bar
 var status_bar = document.getElementById("status_bar");
 status_bar.style.display="none";
+
+// Check for empty fields.
+var txt_song_name = document.getElementById("song_name");
+var txt_song_year = document.getElementById("song_year");
+
+// Add event handler
+txt_song_name.addEventListener("change", Check_Empty);
+txt_song_year.addEventListener("change", Check_Empty);
+
+// Run function
+function Check_Empty() {
+	// Validate song name field.
+	if(txt_song_name.value == "") {
+		document.getElementById("submit_button").style.display = "none";
+	} else {
+		document.getElementById("submit_button").style.display = "";		
+	}
+	
+	// Validate year field.
+	var x, text;
+
+    // Get the value of the input field with id="song_year"
+    x = document.getElementById("song_year").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1900 || x > 2020) {
+        text = "Input not valid";
+		document.getElementById("submit_button").style.display = "none";
+    } else {
+        text = "Input OK";
+		document.getElementById("submit_button").style.display = "";
+    }
+    document.getElementById("status_bar").innerHTML = text;
+	status_bar.style.display="";	
+}
 </script>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/footer.php"); ?>
