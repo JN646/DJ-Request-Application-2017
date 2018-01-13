8c544d2f024ef12fc5073b47b26167bcb4157f79
JN646 <josh.ginn@hotmail.co.uk>
2018-01-13 11:18:06 +0000

    Improvements to Dashboard UI
    Changed from col to cards.

--- admin/index.php
+++ admin/index.php
@@ -29,49 +29,59 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 					<!-- Tools -->
 					<div class="row">
 						<div class="col-md-2">
-							<div class="col-md-12 border">
-								<h3>Sessions</h3>
-								<ul>
-									<li><a href="<?php echo $environment; ?>sessions/sessions_index.php">Session Home</a></li>
+							<div class="card">
+							  <h5 class="card-header">Sessions</h5>
+							  <div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item"><a href="<?php echo $environment; ?>sessions/sessions_index.php">Session Home</a></li>
 								</ul>
+							  </div>
 							</div>
 						</div>
 						<div class="col-md-2">
-							<div class="col-md-12 border">
-								<h3>Songs</h3>
-								<ul>
-									<li><a href="<?php echo $environment; ?>songs/index.php">Song Home</a></li>
-									<li><a href="<?php echo $environment; ?>songs/add_song.php">Song Add</a></li>
-									<li><a href="<?php echo $environment; ?>songs/list_song.php">Song List</a></li>
-									<li><a href="<?php echo $environment; ?>songs/browse_song.php">Browse Songs</a></li>
+							<div class="card">
+							  <h5 class="card-header">Songs</h5>
+							  <div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item"><a href="<?php echo $environment; ?>songs/index.php">Song Home</a></li>
+									<li class="list-group-item"><a href="<?php echo $environment; ?>songs/add_song.php">Song Add</a></li>
+									<li class="list-group-item"><a href="<?php echo $environment; ?>songs/list_song.php">Song List</a></li>
+									<li class="list-group-item"><a href="<?php echo $environment; ?>songs/browse_song.php">Browse Songs</a></li>
 								</ul>
+							  </div>
 							</div>
 						</div>
 						<div class="col-md-2">
-							<div class="col-md-12 border">
-								<h3>Client Devices</h3>
-								<ul>
-									<li><a href="<?php echo $environment; ?>clients/client_index.php">Client Home</a></li>
-									<li><a href="<?php echo $environment; ?>clients/add_client.php">Client Add</a></li>
+							<div class="card">
+							  <h5 class="card-header">Client Devices</h5>
+							  <div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item"><a href="<?php echo $environment; ?>clients/client_index.php">Client Home</a></li>
+									<li class="list-group-item"><a href="<?php echo $environment; ?>clients/add_client.php">Client Add</a></li>
 								</ul>
+							  </div>
 							</div>
 						</div>
 						<div class="col-md-2">
-							<div class="col-md-12 border">
-								<h3>Account</h3>
-								<ul>
-									<li><a href="<?php echo $environment; ?>accounts/accounts_index.php">Client Home</a></li>
-									<li><a href="#">Account Profile</a></li>
+							<div class="card">
+							  <h5 class="card-header">Account</h5>
+							  <div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item"><a href="<?php echo $environment; ?>accounts/accounts_index.php">Client Home</a></li>
+									<li class="list-group-item"><a href="#">Account Profile</a></li>
 								</ul>
+							  </div>
 							</div>
 						</div>
 						<div class="col-md-2">
-							<div class="col-md-12 border">
-								<h3>Zones</h3>
-								<ul>
-									<li><a href="<?php echo $environment; ?>zones/zones_index.php">Zones Home</a></li>
-									<li><a href="<?php echo $environment; ?>zones/add_zone.php">Zones Add</a></li>
+							<div class="card">
+							  <h5 class="card-header">Zones</h5>
+							  <div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item"><a href="<?php echo $environment; ?>zones/zones_index.php">Zones Home</a></li>
+									<li class="list-group-item"><a href="<?php echo $environment; ?>zones/add_zone.php">Zones Add</a></li>
 								</ul>
+							  </div>
 							</div>
 						</div>
 					</div> <!-- Close row -->
