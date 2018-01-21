7770370097d3b7a2cd026644ea06a042d1e10c64
JN646 <josh.ginn@hotmail.co.uk>
2018-01-21 16:28:02 +0000

    Debug Improvements.

--- system/debug.php
+++ system/debug.php
@@ -22,8 +22,10 @@ include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
 					<h1 class="display-4">DEBUG</h1>
 					<div id="status_bar" class="alert alert-warning" role="alert">Please wait while the page loads.</div>
 					<p>This is a debug page. Used to provide system information for feedback and testing.</p>
+					<hr>
 					<?php
 						// Get the User Agent.
+						echo "<h3>Browser Information</h3>";
 						echo $_SERVER['HTTP_USER_AGENT'];
 						
 						// Check for IE and IE11 - Trident based or Edge.
@@ -39,6 +41,40 @@ include($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/partials/header.php");
 							// Using Edge.
 							echo 'You are using Edge.<br />';
 						}
+						
+						// Rule
+						echo "<hr>";
+						
+						// Display Date.
+						echo "<h3>Current Time</h3>";
+						echo "<p>The Current Time Is: <span id='time_span'>Current Time.</span></p>";
+					?>
+					<script type="text/javascript">
+						// Set Variables
+						var currentTime = new Date()
+						var hours = currentTime.getHours()
+						var minutes = currentTime.getMinutes()
+						
+						// include a 0 infront of minutes that fall below 10.
+						if (minutes < 10){
+							minutes = "0" + minutes
+						}
+						
+						// Format with colons.
+						var t_str = hours + ":" + minutes + " ";
+						
+						// Format AM/PM styling.
+						if(hours > 11){
+							t_str += "PM";
+						} else {
+						   t_str += "AM";
+						}
+						
+						// Set Time.
+						document.getElementById('time_span').innerHTML = t_str;
+					</script>
+					<?php
+					
 					?>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
