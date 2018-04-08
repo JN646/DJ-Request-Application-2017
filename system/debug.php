87f1b5a38a7581066ca44155ef69504c3e92f62c
JN646 <josh.ginn@hotmail.co.uk>
2018-04-08 14:43:17 +0100

    Debug and CSS Improvements.

--- system/debug.php
+++ system/debug.php
@@ -33,24 +33,24 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 						// Get the User Agent.
 						echo "<h3>Browser Information</h3>";
 						echo $_SERVER['HTTP_USER_AGENT'];
-						
+
 						// Check for IE and IE11 - Trident based or Edge.
 						if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE ||
 							strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
-							
+
 							// Using IE.
 							echo 'You are using Internet Explorer.<br />';
 						}
-						
+
 						if (strpos($_SERVER['HTTP_USER_AGENT'], 'Edge') !== FALSE) {
-							
+
 							// Using Edge.
 							echo 'You are using Edge.<br />';
 						}
-						
+
 						// Rule
 						echo "<hr>";
-						
+
 						// Display Date.
 						echo "<h3>Current Time</h3>";
 						echo "<p>The Current Time Is: <span id='time_span'>Current Time.</span></p>";
@@ -60,27 +60,26 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){			// If sessi
 						var currentTime = new Date()
 						var hours = currentTime.getHours()
 						var minutes = currentTime.getMinutes()
-						
+
 						// include a 0 infront of minutes that fall below 10.
 						if (minutes < 10){
 							minutes = "0" + minutes
 						}
-						
+
 						// Format with colons.
 						var t_str = hours + ":" + minutes + " ";
-						
+
 						// Format AM/PM styling.
 						if(hours > 11){
 							t_str += "PM";
 						} else {
 						   t_str += "AM";
 						}
-						
+
 						// Set Time.
 						document.getElementById('time_span').innerHTML = t_str;
 					</script>
 					<?php
-					
 					?>
 				</div> <!-- Close col-md-11 -->
 			</div> <!-- Close row -->
