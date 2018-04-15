47ef1b3afe604a6c018442a7ccd5458dee5e4bd6
JN646 <josh.ginn@hotmail.co.uk>
2018-04-15 14:15:01 +0100

    Font Awesome Introduction.

--- partials/header.php
+++ partials/header.php
@@ -20,6 +20,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/djx/djx/classes/class_lib.php");
 
 	<!-- Style Sheets -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo $environment; ?>css/custom.css"> <!-- Custom CSS File -->
 
 	<!-- Favicon -->
