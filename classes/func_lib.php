47f77bc4af488e41c0a7c2f194f84b7e5e61e646
JN646 <josh.ginn@hotmail.co.uk>
2018-04-29 18:57:11 +0100

    Moved more Browse functions to Function Library. Bugs in Browse L pages.

--- classes/func_lib.php
+++ classes/func_lib.php
@@ -105,6 +105,29 @@ function BrowseGenre($mysqli) {
   }
 }
 
+// Add Songs
+function AddSong($mysqli) {
+  if ($_POST['add_song'] == 'Submit') {
+  	//Escape user inputs for security
+  	$song_name = mysqli_real_escape_string($mysqli, $_REQUEST['song_name']);
+  	$song_artist = mysqli_real_escape_string($mysqli, $_REQUEST['song_artist']);
+  	$song_album = mysqli_real_escape_string($mysqli, $_REQUEST['song_album']);
+  	$song_year = mysqli_real_escape_string($mysqli, $_REQUEST['song_year']);
+  	$song_genre = mysqli_real_escape_string($mysqli, $_REQUEST['song_genre']);
+
+  	//Attempt insert query execution
+  	$sql = "INSERT INTO songs (song_name, song_artist, song_album, song_year, song_genre) VALUES ('$song_name', '$song_artist', '$song_album', '$song_year', '$song_genre')";
+  }
+
+  if(mysqli_query($mysqli,$sql))
+  		header("refresh:0; url=../add_song.php");
+  	else
+  		echo "Not added. Something went wrong.";
+
+  // close connection
+  mysqli_close($mysqli);
+}
+
 // GLOBALS
 // Query Error
 function QueryError($query, $mysqli) {
